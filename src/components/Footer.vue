0e549c97f7ff6752626a65f47d6376b976ad49ec
leocarv <leo_1497@hotmail.com>
2022-12-09 00:55:11 -0300

    Primeiros passos para o projeto

---
+++ src/components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="footer">
+        <p>Make Your Burguer &copy; 2022</p>
+    </div>
+</template>
+
+
+
+<script>
+export default {
+    name: "Footer"
+}
+
+</script>
+
+<style scoped> 
+    #footer {
+        height: 200px;
+        background-color: #222;
+        border-top: 4px solid #111;
+        color: #FCBA03;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        bottom: 0;
+        width: 100%;
+    }
+
+</style>
